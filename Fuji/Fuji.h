246b2137bb5a8477027547a078588c159578dfaf
Jack Cook <hello@jackcook.nyc>
2017-08-13 11:08:43 -0700

    Initial commit

---
+++ Fuji/Fuji.h
@@ -0,0 +1,19 @@
+//
+//  Fuji.h
+//  Fuji
+//
+//  Created by Jack Cook on 8/13/17.
+//  Copyright © 2017 Fuji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fuji.
+FOUNDATION_EXPORT double FujiVersionNumber;
+
+//! Project version string for Fuji.
+FOUNDATION_EXPORT const unsigned char FujiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fuji/PublicHeader.h>
+
+
---
+++ Fuji/Fuji.swift
@@ -0,0 +1,9 @@
+//
+//  Fuji.swift
+//  Fuji
+//
+//  Created by Jack Cook on 8/13/17.
+//  Copyright © 2017 Fuji. All rights reserved.
+//
+
+import Foundation
